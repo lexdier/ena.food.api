5c33c7105de422e738be15dc8fb2bd778ab53a46
lexdier <saviolexdier@gmail.com>
2023-07-18 16:40:28 -0300

    feat: add product entity relation

--- src/entities/item.entity.ts
+++ src/entities/item.entity.ts
@@ -2,15 +2,19 @@ import {OrderEntity} from "./order.entity";
 import {Prop, Schema as Collection, SchemaFactory} from "@nestjs/mongoose";
 import mongoose from "mongoose";
 import {AbstractEntity} from "../abstract/abstract.entity";
+import {ProductEntity} from "./product.entity";
 
 @Collection()
-export class ItemEntity extends AbstractEntity{
+export class ItemEntity extends AbstractEntity {
 
     @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'orders'})
     public order: OrderEntity
 
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'products'})
+    public product: ProductEntity
+
     @Prop()
-    public name: string
+    public quantity: number
 
 
 }
