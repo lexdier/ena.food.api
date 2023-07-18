53c069ad800aa2fbb8e661540bf165c614594727
lexdier <saviolexdier@gmail.com>
2023-07-18 16:43:37 -0300

    refactor: add product shop relation

--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -1,8 +1,13 @@
 import {Prop, Schema as Collection, SchemaFactory} from "@nestjs/mongoose";
 import {AbstractEntity} from "../abstract/abstract.entity";
+import mongoose from "mongoose";
+import {ShopEntity} from "./shop.entity";
 
 @Collection()
 export class ProductEntity extends AbstractEntity {
+
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'shops'})
+    public shop: ShopEntity
 
     @Prop()
     public name: string
