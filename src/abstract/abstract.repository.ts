8bc9cd1fff4245dbb4f1a12eae2f02db77fd1aca
lexdier <saviolexdier@gmail.com>
2023-07-18 15:46:47 -0300

    feat: create abstract repository

---
+++ src/abstract/abstract.repository.ts
@@ -0,0 +1,28 @@
+import {Model} from "mongoose";
+import {AbstractEntity} from "./abstract.entity";
+import {Injectable} from "@nestjs/common";
+
+@Injectable()
+export abstract class AbstractRepository {
+
+    protected readonly model: Model<AbstractEntity>
+
+    public find() {
+        return this.model.find()
+    }
+
+    public abstract create(data: any)
+
+    public read() {
+
+    }
+
+    public update() {
+
+    }
+
+    public delete() {
+
+    }
+
+}
