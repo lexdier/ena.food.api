c600e7e93bcc9ac5884f68f99f0dcf4ea5792273
lexdier <saviolexdier@gmail.com>
2023-07-19 17:49:58 -0300

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import {ValidationPipe} from "@nestjs/common";
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+        .setTitle('EnaFood.api')
+        .setDescription('This is the Ena Food API')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    app.useGlobalPipes(new ValidationPipe());
+
+    SwaggerModule.setup('', app, document);
+
+    await app.listen(3000);
 }
+
 bootstrap();
