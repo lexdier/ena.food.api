81d3a031e60e9a7587d4153b715b569b822788a1
lexdier <saviolexdier@gmail.com>
2023-07-19 15:39:59 -0300

    refactor: change model type

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -1,17 +1,20 @@
 import {AbstractRepository} from "../abstract/abstract.repository";
 import {InjectModel} from "@nestjs/mongoose";
 import {Model} from "mongoose";
-import {UserEntity} from "../entities/user.entity";
+import {ProductEntity} from "../entities/product.entity";
 
 export class ProductRepository extends AbstractRepository {
 
     @InjectModel('products')
-    protected readonly model: Model<UserEntity>
+    protected readonly model: Model<ProductEntity>
 
-    public create(data) {
-        const product: UserEntity = new this.model()
+    public create(validator) {
+        const product: ProductEntity = new this.model()
 
-        product.name = data.name
+        product.name = validator.name
+        product.available = validator.available
+        product.shop = validator.shop
+
         return this.model.create(product)
     }
 
