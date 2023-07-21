897f24cd8e9c504fa2f9735c5e3a180bce4d9f4e
lexdier <saviolexdier@gmail.com>
2023-07-21 13:23:46 -0300

    refactor: update product class entity

--- src/entities/cart.entity.ts
+++ src/entities/cart.entity.ts
@@ -2,12 +2,20 @@ import {Prop, Schema as Collection, SchemaFactory} from "@nestjs/mongoose";
 import mongoose from "mongoose";
 import {AbstractEntity} from "../abstract/abstract.entity";
 import {ItemEntity} from "./item.entity";
+import {UserEntity} from "./user.entity";
+import {ShopEntity} from "./shop.entity";
 
 @Collection({timestamps: true})
 export class CartEntity extends AbstractEntity {
 
-    @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'items'})
+    @Prop({type: mongoose.Schema.Types.Array, ref: 'items'})
     public items: ItemEntity[]
+
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'users'})
+    public user: UserEntity
+
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'shops'})
+    public shop: ShopEntity
 
 }
 
